54dc1aea8d46347d78ca4cd809c8187293527653
Manoj Kumar Padhy <manojkumarpadhyms@gmail.com>
2024-04-27 09:08:25 +0530

    geting weather from api completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import { Box } from '@chakra-ui/react';
 import './App.css';
 import Weather from './components/weather';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const getData=async (query)=>{ 
+  try {
+    let data=await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=e2d606b5403f40c685d165141242604&q=${query}&days=1&aqi=no&alerts=no`)
+    console.log(data.data)
+    return data.data
+  } catch (error) {
+    return error
+  }
+}
 
 function App() {
+  let [currentData,setCurrentData] =useState(null)
+  let [location,setLocation]=useState('kolkata')
+
+  useEffect(()=>{
+      getData(location)
+      .then((data)=>{
+          setCurrentData({
+            ...currentData,
+            country:data.location.country,
+            name:data.location.name,
+            date:data.location.localtime,
+            currenttemp:data.current.temp_c,
+            condition:data.current.condition.text,
+            icon:data.current.condition.icon,
+            text:data.current.condition.text,
+            wind:data.current.wind_mph,
+            rain:data.current.cloud,
+            temp_high:data.forecast.forecastday[0].day.maxtemp_c,
+            temp_low:data.forecast.forecastday[0].day.mintemp_c,
+            sunrise:data.forecast.forecastday[0].astro.sunrise,
+            sunset:data.forecast.forecastday[0].astro.sunset,
+            hour:[...data.forecast.forecastday[0].hour]
+
+          })
+      })
+  },[location])
   return (
     <Box className="main_container">
-      <Weather/>
+      {
+        currentData!==null && 
+              <Weather setLocation={setLocation} currentData={currentData}/>
+
+      }
     </Box>
   );
 }
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,44 +1,77 @@
 import { Box, Button, Divider, Flex, Heading, Image, Input, Text, chakra } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import cloudy from './assets/cloudy_1163661.png'
+import { format } from 'date-fns';
 
-export default function Weather() {
+export default function Weather({setLocation,currentData}) {
+    const [search,setSearch]=useState("")
+
+// to convert the date format 
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return format(date, 'EEEE dd MMMM');
+      }
+
+    //   function to convert time 
+    const convertToAmPmTime = (timeString) => {
+        const date = new Date(timeString);
+        return format(date, 'h a');
+      }
+
+    //   function to handel search click 
+    const handelClick=()=>{
+        if(search==="" || search===null)
+        return 
+        setLocation(search)
+        setSearch("")
+    }
     return (
         <>
-            <Box className='location_date' color={'white'} w={'50%'} h={'100px'}>
-                <Heading>London, UK</Heading>
-                <Text>Monday 29 August</Text>
+        {/* location details  */}
+            <Box className='location_date' color={'white'} w={['100%','50%','50%'] } h={'100px'}>
+                <Heading>{`${currentData.name},${currentData.country}`}</Heading>
+                <Text>{formatDate(currentData.date)}</Text>
             </Box>
-            <Box className='search' display={'flex'} alignItems={'top'} w={'50%'} gap={5} h={'100px'}>
-                <Input borderRadius={50} bg={'white'} placeholder='Enter a location...' p={5} />
-                <Button colorScheme='white' bg={'blue.600'} borderRadius={50} border={'1px solid gray'} >Search</Button>
+            {/* search box  */}
+            <Box className='search' display={'flex'} alignItems={'top'} w={['100%','50%','50%']} gap={5} h={'100px'}>
+                <Input
+                onChange={(e)=>{
+                    setSearch(e.target.value)
+                }}
+                value={search}
+                borderRadius={50} bg={'white'} placeholder='Enter a location...' p={5} />
+                <Button 
+                onClick={handelClick}
+                colorScheme='white' bg={'blue.600'} borderRadius={50} border={'1px solid gray'} >Search</Button>
             </Box>
-            <Box class="current-temperature"
+            {/* current temp. details  */}
+            <Box className="current-temperature"
                 h={'200px'}
                 borderRight={'1px solid white'}
                 p={'2%'} display={'flex'} alignItems={'center'} gap={5} w={'30%'} >
-                <Image src={cloudy} w={'100px'} h={'100px'} />
+                <Image src={currentData.icon} w={'100px'} h={'100px'} />
                 <div>
                     <Flex align="center" justify="center" flexDirection="row">
-                        <Text fontSize="70px" color="white">21</Text>
+                        <Text fontSize="70px" color="white">{currentData.currenttemp}</Text>
                         <Text fontSize="70px" color="white">&deg;</Text>
                     </Flex>
-                    <Text color={'white'}>Mostly Sunny</Text>
+                    <Text color={'white'}>{currentData.text}</Text>
                 </div>
-            </Box>
+            </Box>  
             {/* <Box h={'200px'} w={'0.11%'} bg={'white'} ></Box> */}
+            {/* current temp. state */}
             <Box h={'200px'} display={'flex'} justifyContent={'space-between'} w={'30%'} className='current_stats' p={'2%'} >
                 <div>
                     <Flex mb={5} flexDir={'column'} alignItems={'center'}>
                         <Flex align="center" justify="center" flexDirection="row">
-                            <Text fontSize={'xx-large'} color="white">21</Text>
+                            <Text fontSize={'xx-large'} color="white">{currentData.temp_high}</Text>
                             <Text color="white">&deg;</Text>
                         </Flex>
                         <Text color={'GrayText'}>High</Text>
                     </Flex>
                     <Flex flexDir={'column'} alignItems={'center'}>
                         <Flex align="center" justify="center" flexDirection="row">
-                            <Text fontSize={'xx-large'} color="white">21</Text>
+                            <Text fontSize={'xx-large'} color="white">{currentData.temp_low}</Text>
                             <Text color="white">&deg;</Text>
                         </Flex>
                         <Text color={'GrayText'}>Low</Text>
@@ -46,133 +79,138 @@ export default function Weather() {
                 </div>
                 <div>
                     <Flex mb={5} flexDir={'column'} alignItems={'center'}>
-                        <Text color={'white'} fontSize={'xx-large'}>7mph</Text>
+                        <Text color={'white'} fontSize={'xx-large'}>{currentData.wind}</Text>
                         <Text color={'GrayText'}>Wind</Text>
                     </Flex>
                     <Flex flexDir={'column'} alignItems={'center'}>
-                        <Text color={'white'} fontSize={'xx-large'}>0%</Text>
+                        <Text color={'white'} fontSize={'xx-large'}>{currentData.rain}%</Text>
                         <Text color={'GrayText'}>Rain</Text>
                     </Flex>
                 </div>
                 <div>
                     <Flex mb={5} flexDir={'column'} alignItems={'center'}>
-                        <Text color={'white'} fontSize={'xx-large'}>05:27</Text>
+                        <Text color={'white'} fontSize={'xx-large'}>{currentData.sunrise}</Text>
                         <Text color={'GrayText'}>Sunrise</Text>
                     </Flex>
                     <Flex flexDir={'column'} alignItems={'center'}>
-                        <Text color={'white'} fontSize={'xx-large'}>20:57</Text>
+                        <Text color={'white'} fontSize={'xx-large'}>{currentData.sunset}</Text>
                         <Text color={'GrayText'}>Sunset</Text>
                     </Flex>
                 </div>
 
             </Box>
-                <br/>
-            <Box w={'100%'} mt={20} mb={20} className='todays_weather'>
+
+            {/* full day weather details  */}
+            <Box w={'100%'} mt={20} mb={20} id='todays_weather'>
                 <Text color={'white'} mb={3}>Todays weather</Text>
-                <Box display={'flex'} gap={2}  justifyContent={'space-between'}>
-                    <chakra.div display='flex'
-                    flexDir={'column'}
-                    color={'white'}
-                     w={'13%'}
-                    bg={'blue.600'}
-                    borderRadius={5}
-                    p={5}
-                    
-                    gap={2}
-                    alignItems={'center'}>
-                        <Text>3am</Text>
-                        <Image src={cloudy} w={'50px'}/>
-                        <Text>14&deg;</Text>
-                    </chakra.div>
-                    <chakra.div display='flex'
-                    flexDir={'column'}
-                    color={'white'}
-                     w={'13%'}
-                    bg={'blue.600'}
-                    borderRadius={5}
-                    p={5}
-                    
-                    gap={2}
-                    alignItems={'center'}>
-                        <Text>3am</Text>
-                        <Image src={cloudy} w={'50px'}/>
-                        <Text>14&deg;</Text>
-                    </chakra.div>
-                    <chakra.div display='flex'
-                    flexDir={'column'}
-                    color={'white'}
-                     w={'13%'}
-                    bg={'blue.600'}
-                    borderRadius={5}
-                    p={5}
-                    
-                    gap={2}
-                    alignItems={'center'}>
-                        <Text>3am</Text>
-                        <Image src={cloudy} w={'50px'}/>
-                        <Text>14&deg;</Text>
-                    </chakra.div>
-                    <chakra.div display='flex'
-                    flexDir={'column'}
-                    color={'white'}
-                     w={'13%'}
-                    bg={'blue.600'}
-                    borderRadius={5}
-                    p={5}
-                    
-                    gap={2}
-                    alignItems={'center'}>
-                        <Text>3am</Text>
-                        <Image src={cloudy} w={'50px'}/>
-                        <Text>14&deg;</Text>
-                    </chakra.div>
-                    <chakra.div display='flex'
-                    flexDir={'column'}
-                    color={'white'}
-                     w={'13%'}
-                    bg={'blue.600'}
-                    borderRadius={5}
-                    p={5}
-                    
-                    gap={2}
-                    alignItems={'center'}>
-                        <Text>3am</Text>
-                        <Image src={cloudy} w={'50px'} />
-                        <Text>14&deg;</Text>
-                    </chakra.div>   
-                    <chakra.div display='flex'
-                    flexDir={'column'}
-                    color={'white'}
-                     w={'13%'}
-                    bg={'blue.600'}
-                    borderRadius={5}
-                    p={5}
-                    
-                    gap={2}
-                    alignItems={'center'}>
-                        <Text>3am</Text>
-                        <Image src={cloudy} w={'50px'}/>
-                        <Text>14&deg;</Text>
-                    </chakra.div>
-                    <chakra.div display='flex'
-                    flexDir={'column'}
-                    color={'white'}
-                     w={'13%'}
-                    bg={'blue.600'}
-                    borderRadius={5}
-                    p={5}
-                    
-                    gap={2}
-                    alignItems={'center'}>
-                        <Text>3am</Text>
-                        <Image src={cloudy} w={'50px'}/>
-                        <Text>14&deg;</Text>
-                    </chakra.div>
+                <Box display={'flex'} gap={2} rowGap={2}  justifyContent={'space-between'} className='todays_weather'>
+                    {
+                        currentData.hour.map((el,ind)=>{
+                            return ind%3===0 &&ind!=0 &&  (<chakra.div display='flex'
+                            flexDir={ ['row','column','column']}
+                            color={'white'}
+                             w={['100%','13%','13%'] }
+                            bg={'blue.600'}
+                            borderRadius={5}
+                            p={5}
+                            justifyContent={'space-between'}
+                            gap={2}
+                            alignItems={'center'}>
+                                <Text>{convertToAmPmTime(currentData.hour[ind].time)}</Text>
+                                <Image src={currentData.hour[ind].condition.icon} w={'100px'}/>
+                                <Text>{currentData.hour[ind].temp_c}&deg;</Text>
+                            </chakra.div> )
+                        })
+                    }
+                   
                 </Box>
             </Box>
         </>
     )
 }
+{/* <chakra.div display='flex'
+flexDir={'column'}
+color={'white'}
+ w={'13%'}
+bg={'blue.600'}
+borderRadius={5}
+p={5}
 
+gap={2}
+alignItems={'center'}>
+    <Text>3am</Text>
+    <Image src={cloudy} w={'50px'}/>
+    <Text>14&deg;</Text>
+</chakra.div>
+<chakra.div display='flex'
+flexDir={'column'}
+color={'white'}
+ w={'13%'}
+bg={'blue.600'}
+borderRadius={5}
+p={5}
 
+gap={2}
+alignItems={'center'}>
+    <Text>3am</Text>
+    <Image src={cloudy} w={'50px'}/>
+    <Text>14&deg;</Text>
+</chakra.div>
+<chakra.div display='flex'
+flexDir={'column'}
+color={'white'}
+ w={'13%'}
+bg={'blue.600'}
+borderRadius={5}
+p={5}
 
+gap={2}
+alignItems={'center'}>
+    <Text>3am</Text>
+    <Image src={cloudy} w={'50px'}/>
+    <Text>14&deg;</Text>
+</chakra.div>
+<chakra.div display='flex'
+flexDir={'column'}
+color={'white'}
+ w={'13%'}
+bg={'blue.600'}
+borderRadius={5}
+p={5}
+
+gap={2}
+alignItems={'center'}>
+    <Text>3am</Text>
+    <Image src={cloudy} w={'50px'} />
+    <Text>14&deg;</Text>
+</chakra.div>   
+<chakra.div display='flex'
+flexDir={'column'}
+color={'white'}
+ w={'13%'}
+bg={'blue.600'}
+borderRadius={5}
+p={5}
+
+gap={2}
+alignItems={'center'}>
+    <Text>3am</Text>
+    <Image src={cloudy} w={'50px'}/>
+    <Text>14&deg;</Text>
+</chakra.div>
+<chakra.div display='flex'
+flexDir={'column'}
+color={'white'}
+ w={'13%'}
+bg={'blue.600'}
+borderRadius={5}
+p={5}
+
+gap={2}
+alignItems={'center'}>
+    <Text>3am</Text>
+    <Image src={cloudy} w={'50px'}/>
+    <Text>14&deg;</Text>
+</chakra.div>
+
+ */}
